1a2ab0c7cd8292bbbd9e3231f674163004175a35
JTH <jhudspeth24@gmail.com>
2016-06-11 22:36:04 -0500

    Updated linked list implementation

--- src/ll.js
+++ src/ll.js
@@ -1,8 +1,61 @@
-function Node(element)
-{
-	this.data = element;
-	this.next = null;
-
+function Node(value) {
+    this.value = value;
+    this.next = null;
 }
 
-//Sick today 2
+function LL() {
+    this.length = 0;
+    this.head = null;
+}
+
+LL.prototype.insert = function(element) {
+    var node = new Node(element);
+    var curNode = this.head;
+    
+    if(this.head == null) {
+        this.head = node;
+        this.length++;
+        
+        return node;
+    }
+    
+    while(curNode.next) {
+        curNode = curNode.next;
+    }
+    curNode.next = node;
+    this.length++;
+    
+    return node;
+}
+
+LL.prototype.search = function(element) {
+    var curNode = this.head;
+    for(var i = 0; i < this.length; i++) {
+        if(curNode.value == element) {
+            return true;
+        }
+        curNode = curNode.next;
+    }
+    return false;
+}
+
+LL.prototype.print = function() {
+    var curNode = this.head;
+    while(curNode.next != null) {
+        console.log(curNode.value);
+        curNode = curNode.next;
+    }
+    console.log(curNode);
+}
+
+LL.prototype.reverse = function() {
+    var curNode = this.head;
+    var prevNode = null;
+    var tmp = this.head;
+    
+    while(tmp != null) {
+        tmp = tmp.next;
+        curNode.next = prevNode;		
+        prevNode = curNode;
+    }	
+}
